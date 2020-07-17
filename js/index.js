ec0bd737dd429623367bcaa411308e2b042ac0d4
Alla Litovchenko <litovchenko.all@yandex.ru>
2020-07-17 09:38:47 +0300

    сверстала header, main_container

---
+++ js/index.js
@@ -0,0 +1,34 @@
+/* let language = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+};
+
+
+function sendData(inputGroupSelect) {
+    let nameUser = document.getElementById('sendInput').value;
+    let languageUser = document.getElementById('inputGroupSelect').value;
+    console.log(languageUser);
+
+    for (let key in language) {
+        if (key == languageUser) {
+            alert(`${language[key]}, ${nameUser}`);
+        }
+    }
+
+
+} */
